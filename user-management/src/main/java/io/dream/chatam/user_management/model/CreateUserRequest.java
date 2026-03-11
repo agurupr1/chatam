eb7bc877837f12111b5561427d9b6e79134023da
akshayg-hello <akshay.guruprasadh@gmail.com>
2026-03-12 04:42:08 +0530

    init

--- user-management/src/main/java/io/dream/chatam/user_management/model/CreateUserRequest.java
+++ user-management/src/main/java/io/dream/chatam/user_management/model/CreateUserRequest.java
@@ -2,21 +2,12 @@ package io.dream.chatam.user_management.model;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
 
 public class CreateUserRequest {
     @Email(message = "Email should be valid")
     private String email;
     @NotEmpty(message = "Password cannot be empty")
-    @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$",
-            message = "Password must be 8-20 characters long and include upper/lowercase, a number, and a special character"
-    )
     private String password;
-    @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$",
-            message = "Password must be 8-20 characters long and include upper/lowercase, a number, and a special character"
-    )
     @NotEmpty(message = "Re-Password cannot be empty")
     private String rePassword;
 
